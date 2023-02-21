16f54ffd5bc5371ecc1eb8dfe7ef97c631a3a463
Sávio Fialho <savioabfialho@gmail.com>
2023-02-21 20:27:07 -0300

    adding links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export function Navbar() {
   return(
     <header>
       <nav className="max-w-[1360px] m-auto flex justify-between px-20 py-10 text-blue">
         <h1 className="logo text-lg">Dr. Leonardo Marcolino</h1>
         <ul className="flex justify-between gap-10">
-          <li><a href="./">Home</a></li>
-          <li><a href="/#sobre">Sobre</a></li>
-          <li><a href="/#especialidades">Especialidades</a></li>
-          <li><a href="/#contato">Contato</a></li>
+          <li><Link href="./">Home</Link></li>
+          <li><Link href="/#sobre">Sobre</Link></li>
+          <li><Link href="/#especialidades">Especialidades</Link></li>
+          <li><Link href="/#contato">Contato</Link></li>
         </ul>
       </nav>
     </header>
